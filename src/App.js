42754fcbd63c89628a773546081e5a0b7831e222
Jordan Laguio <jordan.laguio@gmail.com>
2022-01-21 13:03:28 -0500

    changed app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React by adding things here
         </a>
       </header>
     </div>
